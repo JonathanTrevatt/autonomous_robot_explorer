96dee1ce91145a4fcc02172a81e93a028f917f7a
Darkspore52 <jonathan@trevatt.net>
2023-09-08 18:51:11 +1000

    added capabilities list, generated a first package and node for learning purposes

---
+++ launch/launch.py
@@ -0,0 +1,28 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='turtlesim',
+            namespace='turtlesim1',
+            executable='turtlesim_node',
+            name='sim'
+        ),
+        Node(
+            package='turtlesim',
+            namespace='turtlesim2',
+            executable='turtlesim_node',
+            name='sim'
+        ),
+        Node(
+            package='turtlesim',
+            executable='mimic',
+            name='mimic',
+            remappings=[
+                # (Topic, remapping)
+                ('/input/pose', '/turtlesim1/turtle1/pose'), # subscription
+                ('/output/cmd_vel', '/turtlesim2/turtle1/cmd_vel'), #publish
+            ]
+        )
+    ])
