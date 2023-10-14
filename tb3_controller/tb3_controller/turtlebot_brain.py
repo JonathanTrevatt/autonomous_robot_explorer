22e33e0ac9dd709a802f10dafeadfaf3e99fb679
Darkspore52 <jonathan@trevatt.net>
2023-10-14 20:00:50 +1000

    Many changes

--- tb3_controller/tb3_controller/turtlebot_brain.py
+++ tb3_controller/tb3_controller/turtlebot_brain.py
@@ -28,8 +28,12 @@ import random
 class Brain(Node):
     def __init__(self):
         super().__init__('brain')
-        self.map = None
-        self.unreachable_positions = []
+        self.unreachable_positions = [] # set to np.zeros((msg.info.height, msg.info.width), dtype=bool) once in map callback
+        # flags that determine if basic data has been instantiated by callbacks yet
+        self.ready_odom = False
+        self.ready_map = False
+        self.ready_log = False
+        self.first_waypoint_sent = False
 
         print('NOTE - turtlebot_brain.Brain: instantiating subscriptions')
         # Subscriber example code:
@@ -37,8 +41,7 @@ class Brain(Node):
         self.status_subscription    = self.create_subscription  (BehaviorTreeLog,           'behavior_tree_log',    self.bt_log_callback,   10)
         self.position_subscription  = self.create_subscription  (Odometry,                  'odom',                 self.odom_callback,     10)
         self.waypoint_publisher     = self.create_publisher     (PoseStamped,               'goal_pose',    10)
-        
-        print("NOTE - turtlebot_brain.Brain: defining qos_profile")
+        self.map_reachable_publisher= self.create_publisher     (OccupancyGrid,             'map_reachable',    10)
 
         qos_profile = QoSProfile(
             reliability=QoSReliabilityPolicy.SYSTEM_DEFAULT,
@@ -46,9 +49,6 @@ class Brain(Node):
             durability=QoSDurabilityPolicy.TRANSIENT_LOCAL,
             depth=1)
 
-
-        print("NOTE - turtlebot_brain.Brain: Initialising navigator")
-        self.first = True
         self.nav = BasicNavigator() # Initialise navigator
         self.nav.lifecycleStartup() #init_pose = self.cur_pos
 
@@ -63,203 +63,213 @@ class Brain(Node):
         self.cur_pos.pose.position.x = self.pos_x
         self.cur_pos.pose.position.y = self.pos_y
         self.cur_pos.pose.orientation.w = self.pos_w
-        if self.first:  
-            self.first = False
-            self.move_to_waypoint(1.0, -0.5, 1)
-            
-    
-    
-    """
-    Converts x,y map pixel coords to global coords in m.
-    """
-    def coord_mapPxl2m(self, mapPos_x, mapPos_y):
-        pos_x = int((mapPos_x * self.mapInfo.resolution) + self.mapInfo.origin.position.x)
-        pos_y = int((mapPos_y * self.mapInfo.resolution) + self.mapInfo.origin.position.y)
-        return pos_x, pos_y
-    
-    """
-    Converts global x,y coords in m to pixel coords on the map.
-    """
-    def coord_m2mapPxl(self, pos_x, pos_y):
-        mapPos_x = int((pos_x - self.mapInfo.origin.position.x)/self.mapInfo.resolution)
-        mapPos_y = int((pos_y - self.mapInfo.origin.position.y)/self.mapInfo.resolution)
-        return mapPos_x, mapPos_y
-    
-    """
-    Returns the current position as the x,y pixel position on the map.
-    """
-    def get_coords_asMapPxl(self):
-        return self.coord_m2mapPxl(self.pos_x, self.pos_y)
-    
-    """
-    Marks a map x,y pixel position as unreachable so that we won't try to pathfind there in future.
-    """
-    def mark_mapPxl_unreachable(self, x, y):
-        x = int(x)
-        y = int(y)
-        self.unreachable_positions.append([x,y])
-        return
-    
-    """
-    Returns a bool: true if given x,y map pixel coords have been previously marked as unreachable - else false.
-    """
-    def is_mapPxl_unreachable(self, x, y):
-        if [x,y] in self.unreachable_positions:
-            return True
-        return False
-
-    """
-    Attempts to generate a path to a waypoint. On failure, returns False, else, True.
-    """
-    def try_generate_path(self, x, y):
-        return True
-
-    #TODO Subscribe to error for unreachable path (in planner_server node)
-
-    # timer_callback for publisher example code
-
-    # listener_callback function for subscriber example code
-
-    
-    """
-    map callback to assign map data to variables
-    Represents a 2-D grid map, in which each cell represents the 
-    probability of occupancy.
-    Values range [-1, 100], where -1 represents an unknown probablility.
-    """
+        if self.ready_map and not self.first_waypoint_sent:  
+            self.first_waypoint_sent = True
+            waypoint = (self.pos_x, self.pos_y, self.pos_w)
+            self.move_to_waypoint(waypoint)
+        self.ready_odom = True
+        
     def map_callback(self, msg:OccupancyGrid):
+        """
+        map callback to assign map data to variables
+        Represents a 2-D grid map, in which each cell represents the 
+        probability of occupancy.
+        Values range [-1, 100], where -1 represents an unknown probablility.
+        """
         print('NOTE - turtlebot_brain.map_callback: reached')
+        print(msg.data)
         self.mapArray2d = np.reshape(msg.data, (-1, msg.info.width))
         self.mapInfo = msg.info
-        self.domap(msg)
+        if self.unreachable_positions == []:
+            self.unreachable_positions = np.zeros((msg.info.height, msg.info.width), dtype=bool)
+        self.ready_map = True
 
-    # If idle, calculate for another waypoint from lab code
+    
     def bt_log_callback(self, msg:BehaviorTreeLog):
-        print('NOTE - turtlebot_brain.bt_log_callback: reached')
         for event in msg.event_log:
             if event.node_name == 'NavigateRecovery' and \
                 event.current_status == 'IDLE':
-                waypoint = self.waypoint_compute(map)
-                self.move_to_waypoint(0.5, 0.5, 1)
+                if self.ready_odom and self.ready_map:
+                    waypointPxl = self.waypointPxl_compute()
+                    print("waypointPxl: ", waypointPxl)
+                    waypoint = self.coord_pxl2m(waypointPxl)
+                    print("waypoint: ", waypoint)
+                    self.move_to_waypoint(waypoint)
+            else: print("robot busy")
+        self.ready_log = True
 
-    # TODO - Detect and react when navigation fails to find a valid path
-    # TODO - Implement strategy for not re-sending bad waypoints
-    def on_exploration_fail(self):
-        print('NOTE - turtlebot_brain.on_exploration_fail: reached')
-        pass
-
-    # TODO - Detect unexplored areas of map
-    def map_find_unexplored(self, map):
-        pass
-
-    # TODO - Generate a trivial waypoint (e.g., step forwards small amount)
-    # To be used for checklist points in event non-trivial solution fails
-    def waypoint_compute_trivial(self):
-        pass
-
-    # TODO - Implement exploration strategy to generate a test waypoint (based on map)
-    def waypoint_compute(self, map):
-        print('NOTE - turtlebot_brain.waypoint_compute: reached')
-        unexplored = self.map_find_unexplored(map) # must navigate robot to unexplored areas
-        waypoint = None
+    def coord_pxl2m(self, waypointPxl):
+        """
+        Converts x,y,w map pixel coords to global coords in m.
+        """
+        mapPos_x, mapPos_y = waypointPxl
+        pos_x = (mapPos_x * self.mapInfo.resolution) + self.mapInfo.origin.position.x
+        pos_y = (mapPos_y * self.mapInfo.resolution) + self.mapInfo.origin.position.y
+        pos_w = 0
+        waypoint = (pos_x, pos_y, pos_w)
         return waypoint
+
+    def coord_m2pxl(self, waypoint):
+        """
+        Converts global x,y coords in m to pixel coords on the map.
+        """
+        pos_x, pos_y, pos_w = waypoint
+        mapPos_x = int((pos_x - self.mapInfo.origin.position.x)/self.mapInfo.resolution)
+        mapPos_y = int((pos_y - self.mapInfo.origin.position.y)/self.mapInfo.resolution)
+        waypointPxl = (mapPos_x, mapPos_y)
+        return waypointPxl
     
-    # TODO - Check if a waypoint is reachable
-    def waypoint_check_reachable(self, waypoint):
-        return True
-
-    # TODO - Low priority - Modify to use python functions instead of just passing a string command directly to terminal.
-    def move_to_waypoint(self, x, y, w):
-        #Use nav2 or custom planning algorithm to move robot to waypoint
-        #This requires sending initial pose and a first waypoint through command line
-        print('NOTE - turtlebot_brain.move_to_waypoint: Setting waypoint {position: {x: %s, y: %s}, orientation: {w: %s}}'  % (x, y, w))
-        os.system("ros2 topic pub -1 /goal_pose geometry_msgs/PoseStamped '{header: {frame_id: 'map'}, pose: {position: {x: %s, y: %s}, orientation: {w: %s}}}'" % (x, y, w))
+    def get_coords_as_Pxl(self):
+        """
+        Returns the current position as the x,y pixel position on the map.
+        """
+        waypoint = (self.pos_x, self.pos_y, self.pos_w)
+        return self.coord_m2pxl(waypoint)
     
+    def mark_range_unreachable(self, pxl, radius):
+        x_posPxl, y_posPxl = pxl
+        xmin = max(x_posPxl-radius, 0)
+        xmax = min(x_posPxl+radius, self.mapInfo.width)
+        ymin = max(y_posPxl-radius, 0)
+        ymax = min(y_posPxl+radius, self.mapInfo.height)
 
-    def map_get_unexplored_in_range(self, mapx, mapy, radius):
-        print("Getting unexplored pixels in range.")
-        unexplored_in_range = []
-        xmin = max(mapx-radius, 0)
-        xmax = min(mapx+radius, self.mapInfo.width)
-        ymin = max(mapy-radius, 0)
-        ymax = min(mapy+radius, self.mapInfo.height)
-
-        print("xmin, xmax, ymin, ymax: ", xmin, xmax, ymin, ymax)
         print("mapArray2d.shape:", self.mapArray2d.shape)
-        for mapx in np.linspace(xmin, xmax, xmax-xmin):
-            for mapy in np.linspace(ymin, ymax, ymax-ymin):
-                mapx=int(mapx)
-                mapy=int(mapy)
-                if self.mapArray2d[mapx][mapy] == -1: # if pixel is unexplored
-                    unexplored_in_range.append([mapx,mapy])
-        return unexplored_in_range
-        
-    def test_unexplored(self, unexplored_list):
-        print("test_unexplored - Testing unexplored map pixels from unexplored_list.")
-        print("test_unexplored - unexplored_list: ", unexplored_list)
-        for i in range(len(unexplored_list)):
-            mapx, mapy = unexplored_list.pop() 
-            if not self.is_mapPxl_unreachable(mapx,mapy):   # if pixel is possibly reachable
-                x, y = self.coord_mapPxl2m(mapx, mapy)
-                if self.try_generate_path(x, y):    # and attempt to generate a path succeeds
-                    if self.move_to_waypoint(x, y, 0): # and waypoint is reached (within time limit)
-                        return True # job done
-                # If we find pixel to be unreachable
-                self.mark_mapPxl_unreachable(mapx,mapy)
-            # If randomly selected pixel is unreachable
-            # try another pixel from list
-        # If every element of the list is unreachable
-        return False
+        for xPxl in np.linspace(xmin, xmax, xmax-xmin):
+            for yPxl in np.linspace(ymin, ymax, ymax-ymin):
+                pxl = (int(xPxl), int(yPxl))
+                self.mark_waypointPxl_unreachable(pxl)
+        self.publish_map_reachable()
 
-    def domap(self, msg:OccupancyGrid):
+    def mark_waypointPxl_unreachable(self, waypointPxl):
         """
-        self.pos_x:  (position of robot in global coords in m)
-        map resolution: each pixel of the map represents 0.05m
-        msg.info.origin.position.x: position of lower left pixel of map (origin) in global coords in m
-        # TODO - implement try_generate_path
-        # TODO - We currently need to wait for move_to_waypoint to complete before marking it as a 
-        success or failure and trying a new pixel. We also currently wait within this function, 
-        which would stop all operations while we wait for movement to complete. We need to fix this.
-        # TODO - ensure we don't generate path twice when we call try_generate_path and 
-        move_to_waypoint (possibly by joining them together as a single function)
+        Marks a map x,y pixel position as unreachable so that we won't try to pathfind there in future.
         """
-        mapx, mapy = self.get_coords_asMapPxl()
+        print("Marking waypoint as unreachable.")
+        xPxl, yPxl = waypointPxl
+        self.unreachable_positions[xPxl][yPxl] = True
+        return
+    
+    def publish_map_reachable(self):
+        grid = OccupancyGrid()
+        a = []
+        for i in range(0, self.mapInfo.height*self.mapInfo.width):
+            a.append(np.random.randint(-1, 1))
+        print("publish_map_reachable!!!!!!!!!!!")
+        grid.data = a
+        grid.info = self.mapInfo
+        self.map_reachable_publisher.publish(grid)
+
+
+    def is_waypointPxl_unreachable(self, waypointPxl):
+        """
+        Returns a bool: true if given x,y map pixel coords have been previously marked as unreachable - else false.
+        """
+        xPxl, yPxl = waypointPxl
+        return self.unreachable_positions[xPxl][yPxl]
+
+    # TODO - implement
+    def try_generate_path(self, waypoint):
+        """
+        Attempts to generate a path to a waypoint. On failure, returns False, else, True.
+        """
+        return True
+    
+    def waypoint_compute_trivial(self):
+        waypoint = (0.5, 0.5, 1)
+        return waypoint
+
+    def waypointPxl_compute(self):
+        """
+        Returns a goal waypoint to pathfind to unexplored areas of the map.
+        """
+        print('NOTE - turtlebot_brain.waypoint_compute: reached')
+        xPxl, yPxl = self.get_coords_as_Pxl()
         min_search_radius = 10
         max_search_radius = 30
         search_radius = min_search_radius
         # search radius for reachable, unexplored pixels and set goal to go there
         while search_radius <= max_search_radius:
-            print("domap - Searching for unexplored pixels in radius: ", search_radius)
+            print("waypoint_compute - Searching for unexplored pixels in radius: ", search_radius)
             # generate list of unexplored pixels within search radius
-            unexplored_list = self.map_get_unexplored_in_range(mapx, mapy, search_radius)
-            print("domap - unexplored_list: ", unexplored_list)
+            unexplored_list = self.map_get_unexplored_in_range(search_radius)
+            print("waypoint_compute - unexplored_list: ", unexplored_list)
             if unexplored_list is not None:
-                print("domap - unexplored_list is not None - unexplored_list = \n", unexplored_list)
+                print("waypoint_compute - unexplored_list is not None - unexplored_list = \n", unexplored_list)
                 print("randomly shuffling unexplored_list to remove preference for exploring in a certain direction.")
                 random.shuffle(unexplored_list)
                 print("Shuffled list = \n", unexplored_list)
-                print("domap - calling self.test_unexplored(unexplored_list)")
-                if self.test_unexplored(unexplored_list):
-                    print("domap - List element unreachable or is already known")
-                    return True # Stop searching
-            print("domap - unexplored_list is None, or every unexplored element is unreachable.")
+                print("waypoint_compute - calling self.waypoint_check_reachable(unexplored_list)")
+                reachable_waypoint_pxl = self.waypoint_check_reachable(unexplored_list)
+                if reachable_waypoint_pxl is not None:
+                    return reachable_waypoint_pxl # Stop searching
+            print("waypoint_compute - unexplored_list is None, or every unexplored element is unreachable.")
             print("Expanding search radius.")
             search_radius += 5 # Expand search radius
-        print("domap - Maximum search radius is reached.")
-        print("domap - Stopping.")
-        return False
+        print("waypoint_compute - Maximum search radius is reached.")
+        print("waypoint_compute - Stopping.")
+        return None # If no valid points are found, return None
 
-        # TODO
-        # check each pxl within range of robot for unexplored pxl
-        # for first unexplored pxl found: 
-        #   check if a path to it can be found
-        #   if a path can be found, set waypoint
-        #   wait of  goal to be reached (or a set delay)
-        # repeat process
-        # if no reachable unexplored pxls in range, increase range and repeat
+    # TODO - Low priority - Use python function instead of terminal command to implement
+    def move_to_waypoint(self, waypoint):
+        """
+        Publishes waypoint for nav2 to guide robot, using terminal commands.
+        """
+        x, y, w = waypoint
+        print("Moving to waypoint: ", waypoint)
+        self.IDLE = False
+        os.system("ros2 topic pub -1 /goal_pose geometry_msgs/PoseStamped '{header: {frame_id: 'map'}, pose: {position: {x: %s, y: %s}, orientation: {w: %s}}}'" % (x, y, w))
+    
+    def move_to_waypointPxl(self, waypointPxl):
+        """
+        Does the same as move_to_waypoint(self, waypoint), but accepts map pixel coordinates.
+        """
+        waypoint = self.coord_pxl2m(waypointPxl)
+        self.move_to_waypoint(waypoint)
 
+    def map_get_unexplored_in_range(self, radius):
+        """
+        Searches a range of map pixels and returns a list of unexplored pixels in that range.
+        """
+        current_posPxl = self.get_coords_as_Pxl()
+        x_posPxl, y_posPxl = current_posPxl
+        print("Getting unexplored pixels in range.")
+        unexplored_in_range = []
+        xmin = max(x_posPxl-radius, 0)
+        xmax = min(x_posPxl+radius, self.mapInfo.width)
+        ymin = max(y_posPxl-radius, 0)
+        ymax = min(y_posPxl+radius, self.mapInfo.height)
 
-
+        print("xmin, xmax, ymin, ymax: ", xmin, xmax, ymin, ymax)
+        print("mapArray2d.shape:", self.mapArray2d.shape)
+        for xPxl in np.linspace(xmin, xmax, xmax-xmin):
+            for yPxl in np.linspace(ymin, ymax, ymax-ymin):
+                xPxl=int(xPxl)
+                yPxl=int(yPxl)
+                pxl = (xPxl, yPxl)
+                if self.mapArray2d[xPxl][yPxl] > 80:
+                    self.mark_range_unreachable(pxl, 5)
+                elif self.mapArray2d[xPxl][yPxl] == -1: # if pixel is unexplored
+                    unexplored_in_range.append(pxl)
+        return unexplored_in_range
+    
+    def waypoint_check_reachable(self, unexplored_list):
+        """
+        Checks a list of unexplored map pxls for a pxl that is not in the list of known unreachable pixels
+        and that a path can be generated to, and returns the coordinates of that pixel.
+        """
+        print("waypoint_check_reachable - Getting unexplored pixel from unexplored_list.")
+        #print("waypoint_check_reachable - unexplored_list: \n", unexplored_list)
+        for i in range(len(unexplored_list)):
+            waypointPxl = unexplored_list.pop()                    # Choose a pixel at random
+            if not self.is_waypointPxl_unreachable(waypointPxl):   # if pixel is not in list of known unreachable pixels
+                waypoint = self.coord_pxl2m(waypointPxl)            
+                if self.try_generate_path(waypoint):               # and if attempt to generate a path succeeds
+                    return waypointPxl                                # Return pixel coords
+                
+                self.mark_waypointPxl_unreachable(waypointPxl)      # Pixel not marked unreachable, but is -> mark as unreachable
+            # Randomly selected pixel is unreachable -> try another
+        # Every element of the list is unreachable -> return None
+        return None
 
 def main(args=None):
     print('NOTE - turtlebot_brain.main: Starting main')
